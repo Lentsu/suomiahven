436303dcaf90d383fe565c440c891970909a47c7
Lenni Toikkanen <lenni.toikkanen@aalto.fi>
2023-07-09 17:14:39 +0300

    commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#   ahven.py
+
+# BEGIN HEADER
+
+""" A Discord bot written for the private Discord server Perch of Finland"""
+
+__author__      = "Lenni Toikkanen"
+__copyright__   = "Free to use"
+
+# END
+    
+# Import libraries
+import os                       # For shell
+import random                   # For randomizing stuff
+import discord                  # For Discord abstractions
+from dotenv import load_dotenv  # For local Tokens
+
+# Mainloop
+def main():
+    client.run('token')
+
+# Only run main if file was ran directly by the interpreter
+if __name__ == __main__:
+    main();
